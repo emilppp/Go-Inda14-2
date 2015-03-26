85b0a5d84cc2e241ca1255b5800754891cd89a4c
Emil Persson <emil.g.persson@gmail.com>
2015-03-26 09:35:15 +0100

    1.1

--- bug1.go
+++ bug1.go
@@ -1,21 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // I want this program to print "Hello world!", but it doesn't work.
 func main() {
 	ch := make(chan string)
 	go func() {
 		ch <- "Hello world!"
-		close(ch)
 	}()
 	fmt.Println(<-ch)
 }
 
 /* All goroutines are asleep - deadlock!
-asd
+In order to send to a channel, it needs to be within a go routine. I simply made a go func() to do the sending.
 
 
 */
